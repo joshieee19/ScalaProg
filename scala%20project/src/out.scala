171980e93683061dae50e89f075fdf3e5cf2436d
joshieee19 <vjosiahedric@yahoo.com>
2016-10-05 11:59:07 +0800

    Add files via upload

---
+++ scala%20project/src/out.scala
@@ -0,0 +1,22 @@
+/**
+  * Created by Villanueva and Sarte
+  */
+object out {
+  def main(args: Array[String]) {
+    import java.io._
+
+    val pw = new PrintWriter(new File("D:\\output.txt" ))
+    val file = scala.io.Source.fromFile("D:\\input.txt")
+    //Sir nakasave po sya sa D://
+      .getLines().toList.flatMap(_.split(" "))
+      .filter(x => x.matches("[a-zA-Z]+"))
+      .sorted.groupBy(x => x)
+      .map(y => (y._1, y._2.length))
+      .toList.sorted
+
+    for (c <- file) {
+      pw.write(c._1 + " - " +c._2 + "\r\n")
+    }
+    pw.close()
+  }
+}
