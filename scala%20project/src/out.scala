a4b2fe99b0a186d694b2c14acf8936316656875f
joshieee19 <vjosiahedric@yahoo.com>
2016-10-06 14:15:44 +0800

    scalaprogv3

--- scala%20project/src/out.scala
+++ scala%20project/src/out.scala
@@ -8,8 +8,8 @@ object out {
     val pw = new PrintWriter(new File("D:\\output.txt" ))
     val file = scala.io.Source.fromFile("D:\\input.txt")
     //Sir nakasave po sya sa D://
-      .getLines().toList.flatMap(_.split(" "))
-      .filter(x => x.matches("[a-zA-Z]+"))
+      .getLines().toList.flatMap(_.split("\\W+"))
+      .filter(x => x.matches("[a-zA-Z]+")&& x.length > 1)
       .sorted.groupBy(x => x)
       .map(y => (y._1, y._2.length))
       .toList.sorted
